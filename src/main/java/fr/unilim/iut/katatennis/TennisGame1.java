7c7e7a52cb0ef7aceea33e74724fd148e0f2f66c
nathan guerin <nathan.guerin@etu.unilim.fr>
2021-10-27 12:03:02 +0200

    Refactored getScore method

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -1,78 +1,54 @@
 package fr.unilim.iut.katatennis;
 
-
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+	private int player1Score = 0;
+	private int player2Score = 0;
+	private String player1Name;
+	private String player2Name;
+	
+	private String[] verbalScore = { "Love", "Fifteen", "Thirty", "Forty" };
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
-    }
+	public TennisGame1(String player1Name, String player2Name) {
+		this.player1Name = player1Name;
+		this.player2Name = player2Name;
+	}
 
-    public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
-    }
+	public void wonPoint(String playerName) {
+		if (playerName.equals(player1Name))
+			player1Score ++;
+		else
+			player2Score ++;
+	}
+
+	public String getScore() {
+		if (player1Score == player2Score)
+			return getScoreForEquality();
+		if (player1Score >= 4 || player2Score >= 4)
+			return getScoreForAdvantageAndWinner();
+		return getStandardScore();
+	}
+
+	private String getStandardScore() {
+		return verbalScore[player1Score] + '-' + verbalScore[player2Score];
+	}
+
+	private String getScoreForAdvantageAndWinner() {
+		String score;
+		int minusResult = player1Score - player2Score;
+		if (minusResult == 1)
+			score = "Advantage " + player1Name;
+		else if (minusResult == -1)
+			score = "Advantage " + player2Name;
+		else if (minusResult >= 2)
+			score = "Win for " + player1Name;
+		else
+			score = "Win for " + player2Name;
+		return score;
+	}
+
+	private String getScoreForEquality() {
+		if (player1Score >= 3) return "Deuce";
+		else return verbalScore[player1Score] + "-All";
+	}
 }
